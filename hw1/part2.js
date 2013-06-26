0d471335248484ba36a829e47c61765d864e2f21
s <s@s-ubuntu.(none)>
2013-06-26 19:14:11 -0400

    Part2

--- hw1/part2.js
+++ hw1/part2.js
@@ -1,6 +1,35 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
+var outfile = "part2.txt";
+
+var testPrime = function(k) {
+    for(var i=2; i<=Math.sqrt(k); i++){
+	if(k%i == 0) return false;
+    }
+    return true;
+}
+
+var list = [];
+
+var i = 0;
+var k = 2;
+
+while(i<100){
+    if(testPrime(k)==true){
+	list.push(k);
+	i++;
+    }
+    k++;
+}
+
+var out="";
+
+for(var i=0; i<list.length; i++){
+    out+=list[i]+",";
+}
+
+out=out.substr(0, out.length-1);
+
+console.log(out);
+
 fs.writeFileSync(outfile, out);
-console.log("Script: "+ __filename + "\nWrote: " + out + "To: " + outfile);
